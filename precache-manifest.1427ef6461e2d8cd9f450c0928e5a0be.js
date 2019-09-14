d2e42147b50df6a10bdfec315bd23c02411d65e2
System Administrator <root@MacBook-Pro-de-Jose-2.local>
2019-09-14 01:14:27 -0500

    Updates

---
+++ precache-manifest.1427ef6461e2d8cd9f450c0928e5a0be.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52c7b5514b2e020c88207f9d5bdc0aa1",
+    "url": "/madkting-test/index.html"
+  },
+  {
+    "revision": "dd031976ba79cdcec470",
+    "url": "/madkting-test/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "d62c48f7804b6fc36925",
+    "url": "/madkting-test/static/css/main.0147a5e5.chunk.css"
+  },
+  {
+    "revision": "dd031976ba79cdcec470",
+    "url": "/madkting-test/static/js/2.efd0fc63.chunk.js"
+  },
+  {
+    "revision": "d62c48f7804b6fc36925",
+    "url": "/madkting-test/static/js/main.d9bfc219.chunk.js"
+  },
+  {
+    "revision": "1399b035ad4c17dec096",
+    "url": "/madkting-test/static/js/runtime~main.145783e1.js"
+  }
+]);
